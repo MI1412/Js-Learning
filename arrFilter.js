8fdf8c6e2ed916b0e14000824c6649935f946b40
MI1412 <imronm1309@gmail.com>
2024-01-05 13:21:30 +0700

    5 januari 2024

---
+++ arrFilter.js
@@ -0,0 +1,8 @@
+// filter sebuah method array yang digunakan untuk mencari elemen array 
+// filter dapat mengembalikan banyak nilai 
+// contoh :
+let angka = [1,2,3,4,5,6,7,8]
+let angka2 = angka.filter(function(x){
+return x < 5;
+})
+console.log(angka2)
---
+++ arrFind.js
@@ -0,0 +1,7 @@
+// find adalah sebuah method array yang dapat mengembalikan nilai hanya 1 
+// contoh :
+let arr = [1,2,3,4,5,6,7,8,9]
+let arr2 = arr.find(function (e){
+return e > 5
+})
+console.log(arr2)
