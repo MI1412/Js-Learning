3f31d420dfda8c2db6c9a006e3521a0ccd6986e9
MI1412 <imronm1309@gmail.com>
2024-01-08 10:35:07 +0700

    8-01-2024

---
+++ object.js
@@ -0,0 +1,16 @@
+ // object pada js adalah kumpulan nilai yang memiliki nama
+// https://www.w3school.com/js/js_object_definition.asp
+// contoh : 
+
+let siswa = {
+nama : "muhammad imron",
+kelas : 1,
+jurusan : "Rpl",
+alamat : {
+	kota : "surabaya",
+	jalan :"jalan abc",
+	nomer : "90",
+	provinsi : "Jawa timur"
+}
+
+}
