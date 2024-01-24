795668e60f478e24c0827128954b93f3098b803f
MI1412 <imronm1309@gmail.com>
2024-01-25 06:09:05 +0700

    belajar dom

--- domSelection.js
+++ domSelection.js
@@ -1,8 +1,28 @@
 // method / fungsi pada dom selection 
-// 1. getElementById() hasil = element
+// 1. document.getElementById() hasil = element
 // 2. getElementsByTagName() hasil = htmlCollection
 // 3. getElementsByClassName() hasil = htmlCollection
 // 4. querySelector() hasil = element
 // 5. querySelectorAll() hasil = nodeList
 
-//
+// contoh :
+// 1.
+const judul = document.getElementById('judul');
+judul.style.color = ('aquamarine')
+judul.style.backgroundColor = ('brown')
+judul.innerHTML = "imron"
+// 2. 
+const p = document.getElementsByTagName('p')
+for(const element of p){
+    element.style.color = ('aquamarine')
+    element.style.backgroundColor = ('red')
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = ('100px')
+// 3.
+const p1 = document.getElementsByClassName('p1')
+for(const element of p1){
+    element.style.backgroundColor = ('green')
+    element.style.color = ('white')
+}
